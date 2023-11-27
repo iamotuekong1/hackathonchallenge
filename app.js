0c398a9acfae9a7b4ec57f899558d68faada80df
iamotuekong1 <iamotuekong1@gmail.com>
2023-11-27 18:34:13 +0100

    Banner Correction

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-// Get the SVG element
-const svgIcon = document.querySelector('#banner svg');
-
-// Add a click event listener to the SVG icon
-svgIcon.addEventListener('click', function () {
-    // Get the section to remove
-    const banner = document.querySelector('#banner');
-
-    // Check if the section exists
-    if (banner) {
-        // Remove the section
-        banner.remove();
-    }
-});
-
-// let topbarMenu = document.getElementById('topbar-menu');
-
-// function toggleTopbarMenu() {
-//     topbarMenu.classList.toggle("open-menu");
-// }
-
-// let alertMenu = document.getElementById('notification');
-
-// function toggleAlertMenu() {
-//     alertMenu.classList.toggle("open-menu");
-// }
 document.addEventListener('DOMContentLoaded', function() {
     var notification = document.querySelector('.notification');
     var alertMenu = document.querySelector('.alert-menu');
     var profile = document.querySelector('.profile');
     var topbarMenu = document.querySelector('.topbar-menu');
+    const svgIcon = document.querySelector('#banner svg');
+    const banner = document.querySelector('#banner');
 
     function toggleAlertMenu() {
         alertMenu.classList.toggle('open-menu');
@@ -46,6 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
         if (event.target !== profile) {
             topbarMenu.classList.remove('open-menu');
         }
+
+        if (banner) {
+            // Remove the section
+            banner.remove();
+        }
     }
 
     notification.addEventListener('click', function(event) {
@@ -61,4 +42,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     document.addEventListener('click', handleDocumentClick);
+
+    svgIcon.addEventListener('click', handleDocumentClick);
 });
