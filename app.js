e20bb5e2617787e8c008f736e5c7ea07cb0bb7dc
iamotuekong1 <iamotuekong1@gmail.com>
2023-11-27 17:17:03 +0100

    Finished Drop Down

--- app.js
+++ app.js
@@ -13,8 +13,52 @@ svgIcon.addEventListener('click', function () {
     }
 });
 
-let topbarMenu = document.getElementById("topbar-menu");
+// let topbarMenu = document.getElementById('topbar-menu');
 
-function toggleTopbarMenu() {
-    topbarMenu.classList.toggle("open-menu");
-}
+// function toggleTopbarMenu() {
+//     topbarMenu.classList.toggle("open-menu");
+// }
+
+// let alertMenu = document.getElementById('notification');
+
+// function toggleAlertMenu() {
+//     alertMenu.classList.toggle("open-menu");
+// }
+document.addEventListener('DOMContentLoaded', function() {
+    var notification = document.querySelector('.notification');
+    var alertMenu = document.querySelector('.alert-menu');
+    var profile = document.querySelector('.profile');
+    var topbarMenu = document.querySelector('.topbar-menu');
+
+    function toggleAlertMenu() {
+        alertMenu.classList.toggle('open-menu');
+    }
+
+    function toggleTopbarMenu() {
+        topbarMenu.classList.toggle('open-menu');
+    }
+
+    function handleDocumentClick(event) {
+        if (event.target !== notification) {
+            alertMenu.classList.remove('open-menu');
+        }
+
+        if (event.target !== profile) {
+            topbarMenu.classList.remove('open-menu');
+        }
+    }
+
+    notification.addEventListener('click', function(event) {
+        event.stopPropagation();
+        toggleAlertMenu();
+        topbarMenu.classList.remove('open-menu');
+    });
+
+    profile.addEventListener('click', function(event) {
+        event.stopPropagation();
+        toggleTopbarMenu();
+        alertMenu.classList.remove('open-menu');
+    });
+
+    document.addEventListener('click', handleDocumentClick);
+});
